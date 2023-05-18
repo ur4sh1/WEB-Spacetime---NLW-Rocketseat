892d62c0037ccf5846ae2e915559432c108f7c67
Lucas <lucas.edgerly@gmail.com>
2023-05-17 22:29:51 -0400

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
 
 export default function Home() {
   return (
-    <div>Teste</div>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/*Left*/}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[228px] w-[526px] -translate-y-1/2 translate-x-1/2 bg-purple-700 rounded-full opacity-50"/>
+
+      </div>
+
+      {/*Right*/}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center ">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">criar agora!</a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
